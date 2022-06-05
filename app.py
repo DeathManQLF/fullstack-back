8eb4c39bad8b0b80018a28d112c56235544bd2aa
DeathMan <ibaimto@gmail.com>
2022-06-06 00:17:07 +0200

    Cambios para pylint

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 @app.route("/pizza", methods=['POST'])
 
 def formulario():
-     """ Redireccion de formulario """
-     
+    """ Redireccion de formulario """
     nombre = request.form.get("nombre")
     apellido = request.form.get("apellido")
     print(nombre, apellido)
--- test_persistencia.py
+++ test_persistencia.py
@@ -1,15 +1,20 @@
 """
 Pruebas Persistencia
+
 """
 def test_guardar_pedido():
+
  """
  Prueba general
+
  """
-with open("pedidos.txt", "w+", encoding="utf-8") as file:
- firstline = file.readline()
-secondline = file.readline(file.close())
-assert firstline == "-Pedro Gil de Diego\n"
-assert secondline == "-Michael Jordan\n"
+ with open("pedidos.txt", "w+", encoding="utf-8") as file:
+
+    firstline = file.readline()
+    secondline = file.readline()
+    file.close()
+    assert firstline == "-Pedro Gil de Diego\n"
+    assert secondline == "-Michael Jordan\n"
 
 test_guardar_pedido("Pedro", "Gil de Diego")
 test_guardar_pedido("Michael", "Jordan")
