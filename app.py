4a63eb0f7659bb3d3de0ee768baed1281182cae3
DeathMan <ibaimto@gmail.com>
2022-06-06 00:06:36 +0200

    Cambios para pylint

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, request, redirect
 app = Flask(__name__)
 
 @app.route("/pizza", methods=['POST'])
+
 def formulario():
-     nombre = request.form.get("nombre")
-     apellido = request.form.get("apellido")
-     print(nombre, apellido)
-     return redirect("http://localhost/PizzaFullStackRelease/FrontEnd/solicita_pedido.html", code=302)
+     """ Redireccion de formulario """
+     
+    nombre = request.form.get("nombre")
+    apellido = request.form.get("apellido")
+    print(nombre, apellido)
+    return redirect("http://localhost/PizzaFullStackRelease/FrontEnd/solicita_pedido.html",code=302)
