800ff54dbc54453d1a6f6cf2fc37149be0cd0daa
DeathMan <ibaimto@gmail.com>
2022-06-06 00:31:39 +0200

    Cambios para pylint

--- test_persistencia.py
+++ test_persistencia.py
@@ -1,20 +1,13 @@
-"""
-Pruebas Persistencia
+""" Pruebas Persistencia """
 
-"""
 def test_guardar_pedido():
-
-    """
-    Prueba general
-
-    """
+    """ Prueba general """
     with open("pedidos.txt", "w+", encoding="utf-8") as file:
-
         firstline = file.readline()
         secondline = file.readline()
         file.close()
         assert firstline == "-Pedro Gil de Diego\n"
         assert secondline == "-Michael Jordan\n"
 
-test_guardar_pedido("Pedro", "Gil de Diego")
-test_guardar_pedido("Michael", "Jordan")
+test_guardar_pedido("Pedro Gil de Diego")
+test_guardar_pedido("Michael Jordan")
