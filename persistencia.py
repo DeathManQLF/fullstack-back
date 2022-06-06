586c526c861eb5141ed681410190abbd2a784685
DeathMan <ibaimto@gmail.com>
2022-06-06 07:42:10 +0200

    cambio para pytest

--- persistencia.py
+++ persistencia.py
@@ -1,6 +1,6 @@
 """ Persistencia """
-
 def guardar_pedido(nombre, apellidos):
+    """ Pedido """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write("-" + nombre + " " + apellidos + "\n")
         file.close()
