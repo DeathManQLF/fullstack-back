9229de38bbaf4d55bdcfce8418645b2451c491b1
DeathMan <ibaimto@gmail.com>
2022-06-06 02:18:55 +0200

    cambio para pytest

--- test_persistencia.py
+++ test_persistencia.py
@@ -9,5 +9,5 @@ def test_guardar_pedido(firstline, secondline):
         assert firstline == "-Pedro Gil de Diego\n"
         assert secondline  == "-Michael Jordan\n"
 
-test_guardar_pedido("-Pedro" "Gil de Diego\n")
-test_guardar_pedido( "-Michael" "Jordan\n")
+test_guardar_pedido("-Pedro" "Gil de Diego ")
+test_guardar_pedido("-Michael" "Jordan ")
