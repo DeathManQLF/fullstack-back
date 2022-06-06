ece504eae0c0d55406af1e2aedeaffebd20d90bb
DeathMan <ibaimto@gmail.com>
2022-06-06 18:03:29 +0200

    Comprueba disponibilidad Backend

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """ Flask """
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 
 app = Flask(__name__)
 
@@ -11,3 +11,17 @@ def formulario():
     apellido = request.form.get("apellido")
     print(nombre, apellido)
     return redirect("http://localhost/PizzaFullStackRelease/FrontEnd/solicita_pedido.html",code=302)
+
+@app.route("/checksize", methods=['POST'])
+    
+def checksize():
+    """ Comprueba disponibilidad de un tamaño de pizza. """
+    tamaño = request.form.get("tamaño")
+    mensaje = 'Lo que corresponda'
+    if (tamaño == 'M' or tamaño =='L' or tamaño =='XXL'):
+        print("Pizza selecionada")
+        return "Disponible"
+    elif (tamaño == 'S'):
+        return "No Disponible, " + mensaje
+
+    return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
