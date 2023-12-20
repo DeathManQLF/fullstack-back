11cf3f12a208c1f853aa5b146bd56707406e0b16
Ibai Montejo <ibaimto@gmail.com>
2023-12-20 04:45:23 +0100

    Metodo Checksize creado

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """ Flask APP """
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 
 app = Flask(__name__)
 
@@ -11,10 +11,21 @@ def index():
 @app.route('/pizza', methods=['POST'])
 def pizza():
     """ Ruta pagina Pizza """
-    nombre = request.form.get('nombre')
-    apellidos = request.form.get('apellidos')
+    nombre = request.form.get("nombre")
+    apellidos = request.form.get("apellidos")
 
     print("Nombre:", nombre)
     print("Apellidos:", apellidos)
 
     return redirect("http://localhost/solicita_pedido.html", code=302)
+
+@app.route("/checksize",methods=['POST'])
+def checksize():
+    """ Comprueba disponibilidad de un tamaño de pizza. """
+    tamano = request.form.get("size")
+    if tamano == "S":
+        mensaje = "No disponible"
+    else:
+        mensaje = "Disponible"
+
+    return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
